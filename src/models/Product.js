ec98f00663ca801c8434658b76c414d0c28e847f
Favio10 <favio_olivera@hotmail.com>
2025-03-28 16:49:44 -0300

    first commit

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,2 +1,33 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+})
+
+module.exports = Product;
